15f097e1ebfcef3859ead1fbc00720bdb04ad2fe
yaqoub akbary <yaqoub2019@gmail.com>
2022-11-08 22:46:32 -0500

    massage

--- src/GitTest/Yaqoub.java
+++ src/GitTest/Yaqoub.java
@@ -3,8 +3,17 @@ package GitTest;
 public class Yaqoub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		   int number1 = 15;
+		   int number2 = 30;
+		   int number3 = 45;
+		   int number4= 70;
+
+		       	if (number1 > number2) {
+		            		System.out.println(number1);
+		            	} else { System.out.println("First number is smaller than second number");
 
 	}
+		       	System.out.println("your a good girl");
 
+}
 }
